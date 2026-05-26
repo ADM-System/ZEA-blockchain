78fae43ed655810626da6c177471c9aaaa372de2
ADM <aaacoadm@gmail.com>
2026-05-26 10:44:50 +0200

    Add files via upload

---
+++ ZEA/Cuore/Crittografia/Crittografia.py
@@ -0,0 +1,16 @@
+#Questo è il file n.1 creato per la blockchain ZEA
+import os
+import hashlib
+import ecdsa
+import base58
+
+def genera_chiave_privata():
+
+def privata_a_pubblica():
+
+def pubblica_a_scripthash():
+
+def scripthash_a_indirizzo():
+
+
+#TEST---CODICE---
